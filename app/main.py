605141609d7b23639e4b5375f7d3e970ce3fca93
miguel <myguel.ra@gmail.com>
2024-02-16 02:33:54 +0000

    add pre-commit

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/temperature") #what will be the temperature in Lisbon in 3 days?
+
+@app.get("/temperature")  # what will be the temperature in Lisbon in 3 days?
 def get_temperature():
     return JSONResponse(content={"temperature": 30})
 
-@app.get("/rain") #Will it rain in Lisbon in 3 days?
+
+@app.get("/rain")  # Will it rain in Lisbon in 3 days?
 def get_rain():
     return JSONResponse(content={"rain": 30})
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,10 +3,12 @@ from fastapi import testclient
 
 client = testclient(app)
 
+
 def test_get_temperature():
     response = client.get("/temperature")
     assert response.status_code == 200
     assert response.json() == {"temperature": 30}
+
 
 def test_get_rain():
     response = client.get("/temperature")
